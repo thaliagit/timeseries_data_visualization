0ffedde049fe72eba3472e6fb12c1b3dbfbecdad
Thalia <thaliatelopoulou@gmail.com>
2024-03-06 21:47:03 +0200

    basic data fetching from db to the frontend table completed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-async function fetchData() {
+export async function fetchData() {
   try {
     const response = await axios.get("http://localhost:3001/data");
-    console.log(response);
+    console.log(response.data);
+    return response.data;
   } catch (error) {
     console.error(error);
   }
 }
-
-export default fetchData;
